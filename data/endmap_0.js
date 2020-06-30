dd307b571748770876121f429126827a7f9e37bb
Guy-Thomas <guyrm.thomas13@gmail.com>
2020-06-30 12:09:58 +0100

    Add files via upload

---
+++ data/endmap_0.js
@@ -0,0 +1,21 @@
+var json_endmap_0 = {
+"type": "FeatureCollection",
+"name": "endmap_0",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 0.0, "Type": "portal", "x": 505.0, "y": -2497.0 }, "geometry": { "type": "Point", "coordinates": [ 0.004536492184804, -0.022430932071477 ] } },
+{ "type": "Feature", "properties": { "id": 1.0, "Type": "EC", "x": -1964.0, "y": -2755.0 }, "geometry": { "type": "Point", "coordinates": [ -0.017642912180107, -0.024748585307911 ] } },
+{ "type": "Feature", "properties": { "id": 2.0, "Type": "EC", "x": 335.0, "y": -1581.0 }, "geometry": { "type": "Point", "coordinates": [ 0.0030093562018, -0.014202364496489 ] } },
+{ "type": "Feature", "properties": { "id": 3.0, "Type": "portal", "x": 1182.0, "y": -1930.0 }, "geometry": { "type": "Point", "coordinates": [ 0.010618086658293, -0.017337484718924 ] } },
+{ "type": "Feature", "properties": { "id": 4.0, "Type": "portal", "x": -497.0, "y": -991.0 }, "geometry": { "type": "Point", "coordinates": [ -0.004464626962074, -0.008902304429806 ] } },
+{ "type": "Feature", "properties": { "id": 5.0, "Type": "portal", "x": -846.0, "y": -4007.0 }, "geometry": { "type": "Point", "coordinates": [ -0.007599747303651, -0.035995491066854 ] } },
+{ "type": "Feature", "properties": { "id": 6.0, "Type": "portal", "x": -703.0, "y": -4549.0 }, "geometry": { "type": "Point", "coordinates": [ -0.00631515644736, -0.040864358810121 ] } },
+{ "type": "Feature", "properties": { "id": 7.0, "Type": "portal", "x": -287.0, "y": -4627.0 }, "geometry": { "type": "Point", "coordinates": [ -0.002578164865423, -0.041565044550448 ] } },
+{ "type": "Feature", "properties": { "id": 8.0, "Type": "EC", "x": 1019.0, "y": -5719.0 }, "geometry": { "type": "Point", "coordinates": [ 0.009153832745178, -0.05137464421465 ] } },
+{ "type": "Feature", "properties": { "id": 9.0, "Type": "portal", "x": 853.0, "y": -3922.0 }, "geometry": { "type": "Point", "coordinates": [ 0.00766262937354, -0.035231923222863 ] } },
+{ "type": "Feature", "properties": { "id": 10.0, "Type": "EC", "x": 1019.0, "y": -3157.0 }, "geometry": { "type": "Point", "coordinates": [ 0.009153832745178, -0.02835981236164 ] } },
+{ "type": "Feature", "properties": { "id": 11.0, "Type": "portal", "x": 1881.0, "y": -1931.0 }, "geometry": { "type": "Point", "coordinates": [ 0.016897310494288, -0.017346467871354 ] } },
+{ "type": "Feature", "properties": { "id": 12.0, "Type": "EC", "x": 1987.0, "y": -1959.0 }, "geometry": { "type": "Point", "coordinates": [ 0.017849524695455, -0.017597996139212 ] } },
+{ "type": "Feature", "properties": { "id": 13.0, "Type": "home", "x": 0.0, "y": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 0.0, 0.0 ] } }
+]
+}
